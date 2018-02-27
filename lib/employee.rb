66e543ed8d6658987ba7428ee3783028a88d1ba1
Evan Wheeler <anon0mys87@gmail.com>
2018-02-27 09:20:19 -0700

    Employee class complete, project class started

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,2 +1,15 @@
 class Employee
+  attr_reader :id,
+              :name,
+              :role,
+              :start_date,
+              :end_date
+
+  def initialize(id, name, role, start_date, end_date)
+    @id = id.to_i
+    @name = name
+    @role = role
+    @start_date = Time.parse(start_date)
+    @end_date = Time.parse(end_date)
+  end
 end
